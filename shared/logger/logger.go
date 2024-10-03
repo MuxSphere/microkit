e557551992c9a9c5f15992e203942552f8e45c06
MuxN4 <stev3npy@gmail.com>
2024-10-03 10:01:06 +0300

    Implemented a shared logger with Zap, including middleware for Gin to log request details and latency

---
+++ shared/logger/logger.go
@@ -0,0 +1,58 @@
+package logger
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+func New(logLevel string) *zap.Logger {
+	level := zap.NewAtomicLevel()
+	level.UnmarshalText([]byte(logLevel))
+
+	config := zap.Config{
+		Level:       level,
+		Development: false,
+		Sampling: &zap.SamplingConfig{
+			Initial:    100,
+			Thereafter: 100,
+		},
+		Encoding:         "json",
+		EncoderConfig:    zap.NewProductionEncoderConfig(),
+		OutputPaths:      []string{"stderr"},
+		ErrorOutputPaths: []string{"stderr"},
+	}
+
+	logger, _ := config.Build()
+	return logger
+}
+
+func GinMiddleware(logger *zap.Logger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		path := c.Request.URL.Path
+		query := c.Request.URL.RawQuery
+
+		c.Next()
+
+		end := time.Now()
+		latency := end.Sub(start)
+
+		if len(c.Errors) > 0 {
+			for _, e := range c.Errors.Errors() {
+				logger.Error(e)
+			}
+		} else {
+			logger.Info(path,
+				zap.Int("status", c.Writer.Status()),
+				zap.String("method", c.Request.Method),
+				zap.String("path", path),
+				zap.String("query", query),
+				zap.String("ip", c.ClientIP()),
+				zap.String("user-agent", c.Request.UserAgent()),
+				zap.Duration("latency", latency),
+			)
+		}
+	}
+}
