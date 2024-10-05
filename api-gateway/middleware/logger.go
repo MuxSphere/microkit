eeed28038aabf2a3c09069114a0134b43f74d73a
MuxN4 <stev3npy@gmail.com>
2024-10-05 11:42:33 +0300

    Logging middleware implementation for HTTP requests

---
+++ api-gateway/middleware/logger.go
@@ -0,0 +1,31 @@
+package middleware
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+func Logger() gin.HandlerFunc {
+	logger, _ := zap.NewProduction()
+	return func(c *gin.Context) {
+		start := time.Now()
+		path := c.Request.URL.Path
+		query := c.Request.URL.RawQuery
+
+		c.Next()
+
+		latency := time.Since(start)
+		status := c.Writer.Status()
+
+		logger.Info("Request",
+			zap.Int("status", status),
+			zap.String("method", c.Request.Method),
+			zap.String("path", path),
+			zap.String("query", query),
+			zap.String("ip", c.ClientIP()),
+			zap.Duration("latency", latency),
+		)
+	}
+}
