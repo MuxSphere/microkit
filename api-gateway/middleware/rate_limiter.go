841022e91b357742e01314bfee2b473e1fe05d8f
MuxN4 <stev3npy@gmail.com>
2024-10-05 11:46:13 +0300

    Rate limiting middleware

---
+++ api-gateway/middleware/rate_limiter.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/time/rate"
+)
+
+func RateLimiter(rps int) gin.HandlerFunc {
+	limiter := rate.NewLimiter(rate.Limit(rps), rps)
+	return func(c *gin.Context) {
+		if !limiter.Allow() {
+			c.JSON(http.StatusTooManyRequests, gin.H{"error": "Too many requests"})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
