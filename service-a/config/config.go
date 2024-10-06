917148e747495548cf84d50ab26e2fdf7b6170a3
MuxN4 <stev3npy@gmail.com>
2024-10-06 11:00:59 +0300

    Consul service discovery configuration

--- service-a/config/config.go
+++ service-a/config/config.go
@@ -13,6 +13,9 @@ type Config struct {
 	DatabaseURL string
 	RabbitMQURL string
 	LogLevel    string
+	ServiceName string
+	Host        string
+	ConsulAddr  string
 }
 
 func Load() (*Config, error) {
@@ -30,6 +33,9 @@ func Load() (*Config, error) {
 	viper.SetDefault("DATABASE_URL", "postgres://user:password@localhost:5432/microservices?sslmode=disable")
 	viper.SetDefault("RABBITMQ_URL", "amqp://guest:guest@localhost:5672/")
 	viper.SetDefault("LOG_LEVEL", "info")
+	viper.SetDefault("SERVICE_NAME", "service-a")
+	viper.SetDefault("HOST", "localhost")
+	viper.SetDefault("CONSUL_ADDR", "consul:8500")
 
 	// Creates and populate Config struct from environment variables
 	var cfg Config
@@ -38,6 +44,9 @@ func Load() (*Config, error) {
 	cfg.DatabaseURL = viper.GetString("DATABASE_URL")
 	cfg.RabbitMQURL = viper.GetString("RABBITMQ_URL")
 	cfg.LogLevel = viper.GetString("LOG_LEVEL")
+	cfg.ServiceName = viper.GetString("SERVICE_NAME")
+	cfg.Host = viper.GetString("HOST")
+	cfg.ConsulAddr = viper.GetString("CONSUL_ADDR")
 
 	return &cfg, nil
 }
